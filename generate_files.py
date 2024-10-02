8769fa144ae62e2c53deb5e888cf31e6b5fdd291
fdzr <fzamora.reina@gmail.com>
2024-10-02 08:56:53 -0300

    changes to generate_file

--- generate_files.py
+++ generate_files.py
@@ -22,50 +22,50 @@ result_per_word = pd.DataFrame(
 correlation_results = pd.DataFrame({"correlation": [], "parameters": []})
 
 methods = ["wsbm"]
-llms = ["wic"]
+llms = ["llama3.1-8B"]
 datasets = ["dwug_es", "dwug_en"]
 
-path_to_folder = Path(f"./cv-apd-experiments-lscd")
+path_to_folder = Path(f"./cv-experiments-lscd-all-hyperparameter")
 if path_to_folder.exists() is False:
     path_to_folder.mkdir()
 
-# for method in methods:
-#     path_to_method = path_to_folder / Path(f"./{method}")
+for method in methods:
+    path_to_method = path_to_folder / Path(f"./{method}")
 
-#     if path_to_method.exists() is False:
-#         path_to_method.mkdir()
+    if path_to_method.exists() is False:
+        path_to_method.mkdir()
 
-for llm in llms:
-    path_to_llm = path_to_folder / llm
+    for llm in llms:
+        path_to_llm = path_to_folder / llm
 
-    if path_to_llm.exists() is False:
-        path_to_llm.mkdir()
+        if path_to_llm.exists() is False:
+            path_to_llm.mkdir()
 
-    for dataset in datasets:
-        path_to_dataset = path_to_llm / dataset
+        for dataset in datasets:
+            path_to_dataset = path_to_llm / dataset
 
-        if path_to_dataset.exists() is False:
-            path_to_dataset.mkdir()
+            if path_to_dataset.exists() is False:
+                path_to_dataset.mkdir()
 
-        path_to_general_results = path_to_dataset / "final_results.txt"
-        if path_to_general_results.exists() is True:
-            path_to_general_results.unlink()
+            path_to_general_results = path_to_dataset / "final_results.txt"
+            if path_to_general_results.exists() is True:
+                path_to_general_results.unlink()
 
-        for fold in range(1, 6):
-            path_to_fold = path_to_dataset / f"{fold}_fold"
+            for fold in range(1, 6):
+                path_to_fold = path_to_dataset / f"{fold}_fold"
 
-            if path_to_fold.exists() is False:
-                path_to_fold.mkdir()
+                if path_to_fold.exists() is False:
+                    path_to_fold.mkdir()
 
-            result_per_word.to_csv(
-                path_to_fold / f"results_training_set.csv", index=False
-            )
-            result_per_word.to_csv(
-                path_to_fold / f"results_testing_set.csv", index=False
-            )
-            correlation_results.to_csv(path_to_fold / "training.csv", index=False)
-            correlation_results.to_csv(path_to_fold / "testing.csv", index=False)
+                result_per_word.to_csv(
+                    path_to_fold / f"results_training_set.csv", index=False
+                )
+                result_per_word.to_csv(
+                    path_to_fold / f"results_testing_set.csv", index=False
+                )
+                correlation_results.to_csv(path_to_fold / "training.csv", index=False)
+                correlation_results.to_csv(path_to_fold / "testing.csv", index=False)
 
-            path_to_verbose_results = path_to_fold / "verbose_results.txt"
-            if path_to_verbose_results.exists() is True:
-                path_to_verbose_results.unlink()
+                path_to_verbose_results = path_to_fold / "verbose_results.txt"
+                if path_to_verbose_results.exists() is True:
+                    path_to_verbose_results.unlink()
