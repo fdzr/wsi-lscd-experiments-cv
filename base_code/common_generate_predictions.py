97aa89f9be73870034e5fdd5e0bc7143a80b232d
fdzr <fzamora.reina@gmail.com>
2024-10-02 08:35:45 -0300

    implementing ari

--- base_code/common_generate_predictions.py
+++ base_code/common_generate_predictions.py
@@ -10,10 +10,14 @@ import unicodedata
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import networkx as nx
 
 from sklearn.preprocessing import MinMaxScaler
+from sklearn import metrics
 from scipy.spatial import distance
 from scipy.stats import spearmanr
+from scipy.sparse.csgraph import laplacian
 
 from custom_types import ShortUse, Results
 from common_extra_processing import create_grouping
@@ -105,6 +109,37 @@ def load_data(path: str, wic_data=False):
     return filtered_data
 
 
+def filter_scored_data(
+    scored_sentence_pair: pd.DataFrame, sense_data: pd.DataFrame, debug=True
+):
+    if debug is True:
+        print(len(scored_sentence_pair), "scores before filtering")
+
+    keys = set(sense_data.identifier + sense_data.word)
+    score_sentence_pair_filtered = scored_sentence_pair[
+        scored_sentence_pair.apply(
+            lambda row: f"{row['identifier1']}{row['word']}", axis=1
+        ).isin(keys)
+        & scored_sentence_pair.apply(
+            lambda row: f"{row['identifier2']}{row['word']}", axis=1
+        ).isin(keys)
+    ]
+
+    if debug is True:
+        print(len(score_sentence_pair_filtered), "scores after filtering")
+
+    return score_sentence_pair_filtered
+
+
+def eigengapHeuristic(adj_matrix, max_n_clusters):
+    L = laplacian(adj_matrix, normed=True)
+    n_components = adj_matrix.shape[0]
+    eigenvalues, eigenvectors = np.linalg.eigh(L)
+    index_largest_gap = np.argsort(np.diff(eigenvalues))[::-1]
+
+    return [x + 1 for x in index_largest_gap if x <= max_n_clusters - 1]
+
+
 def filter_data(data: pd.DataFrame):
     mask1 = data["identifier1"].str.startswith("modern") & data[
         "identifier2"
@@ -180,30 +215,74 @@ def get_gold_data(p: str = "../test_data_es.csv"):
 def save_cv_results(results: dict, metadata: dict = None):
     spr_lscd_training = []
     spr_lscd_testing = []
+    spr_lscd_testing_ari_parameters = []
+    ari_training = []
+    ari_testing = []
+    ari_testing_spr_lscd_parameters = []
 
     path_to_save = f"{metadata['path_to_save_results']}/final_results.txt"
 
     for fold in range(1, 6):
         spr_lscd_training.append(results[fold]["training"]["max_spr_lscd"])
-        spr_lscd_testing.append(results[fold]["testing"])
+        spr_lscd_testing.append(
+            results[fold]["testing"]["optimization_spr_lscd"]["spr_lscd"]
+        )
+        spr_lscd_testing_ari_parameters.append(
+            results[fold]["testing"]["optimization_ari"]["spr_lscd"]
+        )
+
+        ari_training.append(results[fold]["training"]["max_ari"])
+        ari_testing.append(results[fold]["testing"]["optimization_ari"]["ari"])
+        ari_testing_spr_lscd_parameters.append(
+            results[fold]["testing"]["optimization_spr_lscd"]["ari"]
+        )
 
     with open(path_to_save, "a") as f_out:
+        f_out.write(f"Avg ari [train]: {np.array(ari_training).mean()}\n")
+        f_out.write(f"Std ari [train]: {np.array(ari_training).std()}\n")
+        f_out.write(f"Avg ari [test]: {np.array(ari_testing).mean()}\n")
+        f_out.write(f"Std ari [test]: {np.array(ari_testing).std()}\n")
+        f_out.write(f"\n")
+
         f_out.write(f"Avg spr_lscd [train]: {np.array(spr_lscd_training).mean()}\n")
         f_out.write(f"Std spr_lscd [train]: {np.array(spr_lscd_training).std()}\n")
+        f_out.write(f"Avg spr_lscd [test]: {np.array(spr_lscd_testing).mean()}\n")
+        f_out.write(f"Std spr_lscd [test]: {np.array(spr_lscd_testing).std()}\n")
         f_out.write(f"\n")
 
-        f_out.write(f"Avg spr_lscd [testing]: {np.array(spr_lscd_testing).mean()}\n")
-        f_out.write(f"Std spr_lscd [testing]: {np.array(spr_lscd_testing).std()}\n")
+        f_out.write(
+            f"Avg ari [test] - [best parameters for spr_lscd]: {np.array(ari_testing_spr_lscd_parameters).mean()}\n"
+        )
+        f_out.write(
+            f"Std ari [test] - [best parameters for spr_lscd]: {np.array(ari_testing_spr_lscd_parameters).std()}\n"
+        )
         f_out.write(f"\n")
 
-        f_out.write(f"best parameters per fold \n")
+        f_out.write(
+            f"Avg spr_lscd [test] - [best parameters for ari]: {np.array(spr_lscd_testing_ari_parameters).mean()}\n"
+        )
+        f_out.write(
+            f"Std spr_lscd [test] - [best parameters for ari]: {np.array(spr_lscd_testing_ari_parameters).std()}\n"
+        )
+        f_out.write(f"\n")
+
+        f_out.write(f"best parameters per fold ari \n")
         for fold in range(1, 6):
             f_out.write(
-                f"  Fold {fold} {results[fold]['training']['optimal_parameters']}\n"
+                f"  Fold {fold} {results[fold]['training']['optimal_parameters_ari']}\n"
+            )
+
+        f_out.write(f"\n")
+        f_out.write(f"best parameters per fold spr_lscd \n")
+        for fold in range(1, 6):
+            f_out.write(
+                f"  Fold {fold} {results[fold]['training']['optimal_parameters_spr_lscd']}\n"
             )
 
 
-def save_results(word: str, result: Results, parameters: dict, path_to_save: str):
+def save_results(
+    word: str, result: Results, parameters: dict, path_to_save: str, **kwargs
+):
     df = pd.read_csv(path_to_save)
 
     n_rows = df.shape[0]
@@ -214,16 +293,24 @@ def save_results(word: str, result: Results, parameters: dict, path_to_save: str
     df.loc[n_rows, "clusters_to_freq2"] = str(result.cluster_to_freq2)
     df.loc[n_rows, "parameters"] = str(parameters)
 
+    if kwargs:
+        for key in kwargs.keys():
+            df.loc[n_rows, key] = kwargs[key]
+
     df.to_csv(path_to_save, index=False)
 
 
-def save_correlation(correlation: float, parameters: dict, path_to_file: str):
+def save_correlation(correlation: float, parameters: dict, path_to_file: str, **kwargs):
     df = pd.read_csv(path_to_file)
 
     n_rows = df.shape[0]
 
     df.loc[n_rows, "correlation"] = str(correlation)
     df.loc[n_rows, "parameters"] = str(parameters)
+
+    if kwargs:
+        for key in kwargs.keys():
+            df.loc[n_rows, key] = str(kwargs[key])
 
     df.to_csv(path_to_file, index=False)
 
@@ -257,35 +344,36 @@ def get_thresholds(scores: pd.Series):
 def get_predictions(
     get_clusters: typing.Callable,
     scores: pd.DataFrame,
+    senses: pd.DataFrame,
     hyperparameter_combinations: typing.List[dict],
     metadata: dict = None,
 ):
     logging.info("get predictions ...")
-    words = scores.word.unique()
+    words = senses.word.unique()
     jsd = {}
+    ari_silhouette = 0.0
 
     kfold = metadata["kfold"]
     name_file = metadata["name_file"]
 
     for word in words:
-        mask = scores["word"] == word
-        filtered_scores = scores[mask]
+        word_scores = scores[scores["word"] == word]
+        word_senses = senses[senses["word"] == word]
 
-        ids = set(filtered_scores["identifier1"].to_list()).union(
-            set(filtered_scores["identifier2"].to_list())
-        )
-
+        ids = word_senses["identifier"].to_list()
         grouping = pd.DataFrame({"ids": list(ids)})
         grouping["grouping"] = grouping.apply(
             lambda row: 1 if row["ids"].startswith("old") else 2, axis=1
         )
 
-        context = [ShortUse(word=word, id=id) for id in ids]
-        n_sentences = len(ids)
+        context = word_senses.apply(
+            lambda row: ShortUse(word=row["word"], id=row["identifier"]), axis=1
+        ).tolist()
+        n_sentences = len(context)
 
         id2int = {value: index for index, value in enumerate(context)}
         adj_matrix = get_adj_matrix(
-            filtered_scores,
+            word_scores,
             id2int,
             n_sentences,
             hyperparameter_combinations["fill_diagonal"],
@@ -295,27 +383,65 @@ def get_predictions(
             wic_score=metadata["wic_data"],
         )
 
-        logging.info("calculating predictions ...")
-        clusters = get_clusters(
-            adj_matrix, hyperparameter_combinations["model_hyperparameters"]
-        )
-        pred_clusters = {c.id: clusters[id2int[c]] for index, c in enumerate(context)}
+        clusterings = []
+        silhouettes = []
+        cluster_rand_score = {
+            ncluster: 0.0 for ncluster in range(2, metadata["max_n_clusters"] + 1)
+        }
+        eigengap_n_clusters = eigengapHeuristic(adj_matrix, metadata["max_n_clusters"])
+
+        for n_cluster in range(2, metadata["max_n_clusters"] + 1):
+            hyperparameter_combinations["model_hyperparameters"][
+                "n_clusters"
+            ] = n_cluster
+
+            logging.info("calculating predictions ...")
+            clusters = get_clusters(
+                adj_matrix, hyperparameter_combinations["model_hyperparameters"]
+            )
+
+            clusterings.append(clusters)
+            silhouettes.append(
+                metrics.silhouette_score(adj_matrix, clusters, metric="euclidean")
+            )
+
+            clusters_predicted = word_senses.apply(
+                lambda row: clusters[
+                    id2int[ShortUse(word=row["word"], id=row["identifier"])]
+                ],
+                axis=1,
+            ).tolist()
+
+            rand = metrics.adjusted_rand_score(
+                word_senses["cluster"], clusters_predicted
+            )
+            cluster_rand_score[n_cluster] += rand
+
+        number_clusters = np.array(silhouettes).argmax() + 2
+        labels = clusterings[number_clusters - 2]
+        pred_clusters = {c.id: labels[id2int[c]] for _, c in enumerate(context)}
         jsd[word] = compute_jsd(pred_clusters, grouping)
+        ari_silhouette += cluster_rand_score[number_clusters]
+        hyperparameter_combinations["model_hyperparameters"][
+            "n_clusters"
+        ] = number_clusters
         save_results(
             word,
             jsd[word],
             hyperparameter_combinations,
             f"{metadata['path_to_save_results']}/{kfold}_fold/{name_file}.csv",
+            ari=cluster_rand_score[number_clusters],
         )
 
     logging.info("returning predictions ...")
 
-    return jsd
+    return jsd, ari_silhouette / len(words)
 
 
 def get_predictions_without_nclusters(
     get_clusters: typing.Callable,
     scores: pd.DataFrame,
+    senses: pd.DataFrame,
     hyperparameter_combinations: typing.List[dict],
     metadata: dict = None,
 ):
@@ -381,11 +507,14 @@ def get_predictions_without_nclusters(
 def eval(
     get_clusters: typing.Callable,
     scores: dict[str, pd.DataFrame],
+    senses: pd.DataFrame,
     test_set: list,
     parameters: dict,
     metadata: dict,
 ):
     logging.info(f"eval {metadata['method']} method...")
+
+    test_senses = senses[senses["word"].isin(test_set)]
 
     metadata["name_file"] = "results_testing_set"
 
@@ -402,12 +531,20 @@ def eval(
         test_scores = score_filtered[score_filtered["word"].isin(test_set)]
 
         if metadata["method"] in ["ac", "sc"]:
-            jsd = get_predictions(
-                get_clusters, test_scores, hyperparameters, metadata=metadata
+            jsd, ari = get_predictions(
+                get_clusters,
+                test_scores,
+                test_senses,
+                hyperparameters,
+                metadata=metadata,
             )
         else:
-            jsd = get_predictions_without_nclusters(
-                get_clusters, test_scores, hyperparameters, metadata=metadata
+            jsd, ari = get_predictions_without_nclusters(
+                get_clusters,
+                test_scores,
+                test_senses,
+                hyperparameters,
+                metadata=metadata,
             )
 
         logging.info("  calculating correlation ...")
@@ -419,30 +556,36 @@ def eval(
             spr,
             hyperparameters,
             f"{metadata['path_to_save_results']}/{metadata['kfold']}_fold/testing.csv",
+            avg_ari=ari,
         )
         logging.info("  results saved ...")
 
         metadata.pop("scaler", None)
         metadata.pop("threshold", None)
 
-        return spr
+        return spr, ari
 
 
 def train(
     get_clusters: typing.Callable,
-    scores: pd.DataFrame,
+    scores: dict[str, pd.DataFrame],
+    senses: pd.DataFrame,
     train_set: list,
     hyperparameter_combinations: list,
     metadata: dict = None,
 ):
     method = metadata["method"]
-    optimal_parameters = None
+    optimal_parameters_spr_lscd = None
+    optimal_parameters_ari = None
     max_spr_lscd = float("-inf")
+    max_ari = float("-inf")
 
     metadata.update({"name_file": "results_training_set"})
 
     logging.info(f"training {method} method ...")
     number_iterations = len(hyperparameter_combinations)
+
+    train_senses = senses[senses["word"].isin(train_set)]
 
     for index, hyperparameter in enumerate(hyperparameter_combinations):
         logging.info(f"  {index + 1}/{number_iterations} - {hyperparameter}")
@@ -461,8 +604,12 @@ def train(
 
         if method in ["ac", "sc"]:
             try:
-                jsd = get_predictions(
-                    get_clusters, train_scores, hyperparameter, metadata=metadata
+                jsd, ari = get_predictions(
+                    get_clusters,
+                    train_scores,
+                    train_senses,
+                    hyperparameter,
+                    metadata=metadata,
                 )
             except Exception as e:
                 logging.error(f"error processing parameters: {hyperparameter}")
@@ -471,7 +618,7 @@ def train(
                 continue
         else:
             try:
-                jsd = get_predictions_without_nclusters(
+                jsd, ari = get_predictions_without_nclusters(
                     get_clusters, train_scores, hyperparameter, metadata=metadata
                 )
             except Exception as e:
@@ -492,24 +639,43 @@ def train(
             spr,
             hyperparameter,
             f"{metadata['path_to_save_results']}/{metadata['kfold']}_fold/training.csv",
+            avg_ari=ari,
         )
         logging.info("  results saved ...")
 
-        if spr > max_spr_lscd:
-            max_spr_lscd = spr
-            optimal_parameters = copy.deepcopy(hyperparameter)
+        if ari > max_ari:
+            max_ari = ari
+            optimal_parameters_ari = copy.deepcopy(hyperparameter)
 
             if metadata["wic_data"] is True:
-                optimal_parameters["scaler"] = scaler
-                optimal_parameters["threshold"] = thresholds[hyperparameter["quantile"]]
+                optimal_parameters_ari["scaler"] = scaler
+                optimal_parameters_ari["threshold"] = thresholds[
+                    hyperparameter["quantile"]
+                ]
 
-    return {"max_spr_lscd": max_spr_lscd, "optimal_parameters": optimal_parameters}
+        if spr > max_spr_lscd:
+            max_spr_lscd = spr
+            optimal_parameters_spr_lscd = copy.deepcopy(hyperparameter)
+
+            if metadata["wic_data"] is True:
+                optimal_parameters_spr_lscd["scaler"] = scaler
+                optimal_parameters_spr_lscd["threshold"] = thresholds[
+                    hyperparameter["quantile"]
+                ]
+
+    return {
+        "max_spr_lscd": max_spr_lscd,
+        "optimal_parameters_spr_lscd": optimal_parameters_spr_lscd,
+        "max_ari": max_ari,
+        "optimal_parameters_ari": optimal_parameters_ari,
+    }
 
 
 def cross_validation(
     hyperparameter_combinations: list,
     get_clusters: typing.Callable,
-    scores: pd.DataFrame,
+    scores: dict[str, pd.DataFrame],
+    senses: pd.DataFrame,
     metadata: dict = None,
 ):
     dataset = metadata["dataset"]
@@ -524,34 +690,79 @@ def cross_validation(
         configuration = train(
             get_clusters,
             scores,
+            senses,
             train_set,
             hyperparameter_combinations,
             metadata=metadata,
         )
 
-        spr = eval(
+        spr_best_parameters_ari, ari_best_parameters_ari = eval(
             get_clusters,
             scores,
+            senses,
             test_set,
-            configuration["optimal_parameters"],
+            configuration["optimal_parameters_ari"],
             metadata,
         )
 
-        results[index] = {"training": configuration, "testing": spr}
+        spr_best_parameters_spr_lscd, ari_best_parameters_spr_lscd = eval(
+            get_clusters,
+            scores,
+            senses,
+            test_set,
+            configuration["optimal_parameters_spr_lscd"],
+            metadata,
+        )
+
+        results[index] = {
+            "training": configuration,
+            "testing": {
+                "optimization_ari": {
+                    "ari": ari_best_parameters_ari,
+                    "spr_lscd": spr_best_parameters_ari,
+                },
+                "optimization_spr_lscd": {
+                    "ari": ari_best_parameters_spr_lscd,
+                    "spr_lscd": spr_best_parameters_spr_lscd,
+                },
+            },
+        }
 
         path_to_save = (
             f"{metadata['path_to_save_results']}/{index}_fold/verbose_results.txt"
         )
         with open(path_to_save, "a") as f_out:
-            f_out.write("best parameters for training: \n")
-            f_out.write(f"  {configuration['optimal_parameters']}\n")
+            f_out.write("training - best parameters for ari: \n")
+            f_out.write(f"  {configuration['optimal_parameters_ari']}\n")
+            f_out.write(f"training [ari]: \n")
+            f_out.write(f"  {configuration['max_ari']}\n")
+
+            f_out.write("\n")
+
+            f_out.write("training - best parameters for spr_lscd: \n")
+            f_out.write(f"  {configuration['optimal_parameters_spr_lscd']}\n")
             f_out.write(f"training [spr_lscd]: \n")
             f_out.write(f"  {configuration['max_spr_lscd']}\n")
 
             f_out.write("\n")
 
+            f_out.write(f"testing [ari]:\n")
+            f_out.write(f"  {ari_best_parameters_ari}")
+
+            f_out.write("\n")
+
             f_out.write(f"testing [spr_lscd]:\n")
-            f_out.write(f"  {spr}")
+            f_out.write(f"  {spr_best_parameters_spr_lscd}")
+
+            f_out.write("\n")
+
+            f_out.write(f"testing [ari] - [best parameters for spr_lscd]:\n")
+            f_out.write(f"  {ari_best_parameters_spr_lscd}")
+
+            f_out.write("\n")
+
+            f_out.write(f"testing [spr_lscd] - [best parameters for ari]:\n")
+            f_out.write(f"  {spr_best_parameters_ari}")
 
     return results
 
@@ -586,20 +797,22 @@ def grid_search(
     metadata: dict = None,
 ):
     scores = {}
+    senses = pd.read_csv(f"{metadata['path_to_sense_data']}", sep="\t")
+
     for sp in metadata["score_paths"]:
-        scores[sp] = load_data(
+        data = load_data(
             f"{metadata['path_to_data']}/{sp}", wic_data=metadata["wic_data"]
         )
+        scores[sp] = filter_scored_data(data, senses)
 
     if metadata["dataset"] == "dwug_en":
         create_grouping(scores, metadata["score_paths"], logging)
-
-    # scores = get_data()
 
     results = cross_validation(
         hyperparameter_combinations,
         get_clusters,
         scores,
+        senses,
         metadata=metadata,
     )
 
